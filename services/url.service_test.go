73c3dfa5fc6bd6106358e68bb6d8b8e8050bf3ef
Sumit Jadhav (Thouhtworks, IT, KLI) <kli.sumit-jadhav@kotak.com>
2024-03-15 17:26:15 +0530

    Add url service unit tests

--- services/url.service_test.go
+++ services/url.service_test.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	"github.com/sumitsj/url-shortener/models"
 	"github.com/sumitsj/url-shortener/repositories/mocks"
 	mocks2 "github.com/sumitsj/url-shortener/services/mocks"
 	"testing"
@@ -27,8 +28,84 @@ func TestUrlService_GenerateShortUrl(t *testing.T) {
 
 	assert.Contains(t, shortUrl, expectedShortUrl)
 	assert.Equal(t, 30, len(shortUrl))
-	repository.AssertNumberOfCalls(t, "Create", 1)
-	repository.AssertNumberOfCalls(t, "GetByOriginalUrl", 1)
-	appConfig.AssertNumberOfCalls(t, "GetServerAddr", 1)
-	appConfig.AssertNumberOfCalls(t, "GetServerPort", 1)
+	repository.AssertExpectations(t)
+	appConfig.AssertExpectations(t)
+}
+
+func TestUrlService_GenerateShortUrl_ShouldReturnExistingShortUrlIfExists(t *testing.T) {
+	url := "www.google.com"
+	expectedShortUrl := "http://localhost:8080/s/abcdef"
+
+	repository := mocks.NewUrlMappingRepository(t)
+	repository.On("GetByOriginalUrl", url).Return(&models.URLMapping{
+		OriginalUrl:  url,
+		ShortenedUrl: expectedShortUrl,
+	}, nil)
+
+	appConfig := mocks2.NewAppConfig(t)
+
+	service := CreateUrlService(appConfig, repository)
+
+	shortUrl, _ := service.GenerateShortUrl(url)
+
+	assert.Equal(t, expectedShortUrl, shortUrl)
+	repository.AssertNumberOfCalls(t, "Create", 0)
+	repository.AssertExpectations(t)
+}
+
+func TestUrlService_GenerateShortUrl_ShouldReturnErrorInCaseOfDbIssue(t *testing.T) {
+	url := "www.google.com"
+
+	repository := mocks.NewUrlMappingRepository(t)
+	repository.On("GetByOriginalUrl", url).Return(nil, errors.New("document not found"))
+	repository.On("Create", mock.AnythingOfType("*models.URLMapping")).Return(errors.New("connection failed"))
+
+	appConfig := mocks2.NewAppConfig(t)
+	appConfig.On("GetServerAddr").Return("http://localhost")
+	appConfig.On("GetServerPort").Return("8080")
+
+	service := CreateUrlService(appConfig, repository)
+
+	shortUrl, err := service.GenerateShortUrl(url)
+
+	assert.Equal(t, "", shortUrl)
+	assert.Error(t, err)
+	assert.Equal(t, "Failed to save url mapping for url: www.google.com.\nError: connection failed", err.Error())
+}
+
+func TestUrlService_GetOriginalUrlBy(t *testing.T) {
+	url := "www.google.com"
+	shortenedUrl := "http://localhost:8080/s/abcdef"
+
+	repository := mocks.NewUrlMappingRepository(t)
+	repository.On("GetByShortenedUrl", shortenedUrl).Return(&models.URLMapping{
+		OriginalUrl:  url,
+		ShortenedUrl: shortenedUrl,
+	}, nil)
+
+	appConfig := mocks2.NewAppConfig(t)
+	service := CreateUrlService(appConfig, repository)
+
+	originalUrl, _ := service.GetOriginalUrlBy(shortenedUrl)
+
+	assert.Equal(t, url, originalUrl)
+	repository.AssertExpectations(t)
+}
+
+func TestUrlService_GetOriginalUrlBy_ShouldReturnErrorInCaseOfDbIssue(t *testing.T) {
+	shortenedUrl := "http://localhost:8080/s/abcdef"
+	errorMessage := "db error"
+
+	repository := mocks.NewUrlMappingRepository(t)
+	repository.On("GetByShortenedUrl", shortenedUrl).Return(nil, errors.New(errorMessage))
+
+	appConfig := mocks2.NewAppConfig(t)
+	service := CreateUrlService(appConfig, repository)
+
+	originalUrl, err := service.GetOriginalUrlBy(shortenedUrl)
+
+	assert.Equal(t, "", originalUrl)
+	assert.Error(t, err)
+	assert.Equal(t, errorMessage, err.Error())
+	repository.AssertExpectations(t)
 }
